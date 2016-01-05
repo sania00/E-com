6253eb7b5daec6990c73b415f8ff0145adfd7641
sania00 <sania_00@mail.ru>
2016-01-05 10:34:44 +0500

    new

--- config/routes.php
+++ config/routes.php
@@ -2,6 +2,7 @@
 
 return array(
 
-    ''      => 'site/index', //actionIndex в SiteController
+    'product/(0-9)+'    => 'product/view/$1', //actionView в ProductController
+    ''                  => 'site/index', //actionIndex в SiteController
 
 );
