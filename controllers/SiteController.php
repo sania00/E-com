21fdbec70b2ed26b37abec93335ff7306e5dc94f
sania00 <sania_00@mail.ru>
2016-01-05 10:22:12 +0500

    new

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -4,7 +4,7 @@ class SiteController{
 
     public function actionIndex(){
 
-        require_once (ROOT. '/views/site/index.php');
+        require_once(ROOT. '/views/site/index.php');
 
         return true;
 
